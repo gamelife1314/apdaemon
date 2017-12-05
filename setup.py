92aaa5c88b2252d2740227d06ad5551085108546
付登龙 <1185694600@qq.com>
2017-12-05 13:34:04 +0800

    fix: process status display error.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [
 
 setup(
     name='apdaemon',
-    version='0.2.5',
+    version='0.2.6',
     packages=find_packages(),
     url='https://github.com/gamelife1314/apdaemon',
     license='MIT',
